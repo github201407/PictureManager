68340ae6df08da8d99cd5c57cdd5d5e2ba667a2c
github201407 <541788847@qq.com>
2016-03-16 08:39:12 +0800

    init

---
+++ app/src/main/java/com/jen/timeless/adapter/PictureAdapter.java
@@ -0,0 +1,32 @@
+package com.jen.timeless.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by jen on 2016/3/16.
+ */
+public class PictureAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+
+    public PictureAdapter(Context context) {
+    }
+
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
